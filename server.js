8a5ec22c5b64aa0af8666b0e6012ccf66bbba88b
Pavel Kondratev <pablo.osamu@yandex.ru>
2022-07-20 15:32:26 +0400

    try opload

--- server.js
+++ server.js
@@ -1,19 +1,9 @@
-function requireHTTPS(req, res, next) {
-    // The 'x-forwarded-proto' check is for Heroku
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-        return res.redirect('https://' + req.get('host') + req.url);
-    }
-    next();
-}
-
 const express = require('express');
 const app = express();
+const path = require('path')
 
-app.use(requireHTTPS);
-app.use(express.static('./dist/Front-end'));
-
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/Front-end/'}),
-);
-
+app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 8080);
+app.get('/*', (req, res) =>
+    res.sendFile(path.join(__dirname + '/dist/index.html')),
+);
